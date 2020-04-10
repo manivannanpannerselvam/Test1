f8b686679dbb4471ab42d64192e983c40587cda5
Mani <manipinnai238@gmail.com>
2020-04-10 12:24:48 +0800

    Helper and IMDB page

--- Cucumber/src/main/java/com/cucumber/framework/helper/TestBase/TestBase.java
+++ Cucumber/src/main/java/com/cucumber/framework/helper/TestBase/TestBase.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.log4j.Logger;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.NoSuchElementException;
@@ -14,7 +14,8 @@ import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
@@ -26,13 +27,10 @@ import com.cucumber.framework.configreader.ObjectRepo;
 import com.cucumber.framework.configreader.PropertyFileReader;
 import com.cucumber.framework.configuration.browser.BrowserType;
 import com.cucumber.framework.configuration.browser.ChromeBrowser;
-import com.cucumber.framework.configuration.browser.FirefoxBrowser;
-import com.cucumber.framework.configuration.browser.HtmlUnitBrowser;
-import com.cucumber.framework.configuration.browser.IExploreBrowser;
-import com.cucumber.framework.helper.Logger.LoggerHelper;
+
 import com.cucumber.framework.utility.DateTimeHelper;
 import com.cucumber.framework.utility.ResourceHelper;
-import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+
 import com.google.common.base.Function;
 
 import cucumber.api.Scenario;
@@ -49,17 +47,17 @@ import cucumber.api.java.Before;
  */
 public class TestBase {
 
-	private final Logger log = LoggerHelper.getLogger(TestBase.class);
+	//private final Logger log = LoggerHelper.getLogger(TestBase.class);
 	
 	public static WebDriver driver;
 
-	public void waitForElement(WebElement element, int timeOutInSeconds) {
-		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+	public void waitForElement(WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, 9000000);
 		wait.ignoring(NoSuchElementException.class);
 		wait.ignoring(ElementNotVisibleException.class);
 		wait.ignoring(StaleElementReferenceException.class);
-		wait.ignoring(ElementNotFoundException.class);
-		wait.pollingEvery(250, TimeUnit.MILLISECONDS);
+		wait.ignoring(ElementNotVisibleException.class);
+		wait.pollingEvery(300, TimeUnit.SECONDS);
 		wait.until(elementLocated(element));
 	}
 
@@ -67,100 +65,29 @@ public class TestBase {
 		return new Function<WebDriver, Boolean>() {
 
 			public Boolean apply(WebDriver driver) {
-				log.debug("Waiting for Element : " + element);
+				//log.debug("Waiting for Element : " + element);
 				return element.isDisplayed();
 			}
 		};
+	
 	}
-
-	public String takeScreenShot(String name) throws IOException {
-
-		if (driver instanceof HtmlUnitDriver) {
-			log.fatal("HtmlUnitDriver Cannot take the ScreenShot");
-			return "";
-		}
-
-		File destDir = new File(ResourceHelper.getResourcePath("screenshots/") + DateTimeHelper.getCurrentDate());
-		if (!destDir.exists())
-			destDir.mkdir();
-
-		File destPath = new File(destDir.getAbsolutePath() + System.getProperty("file.separator") + name + ".jpg");
-		try {
-			FileUtils.copyFile(((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE), destPath);
-		} catch (IOException e) {
-			log.error(e);
-			throw e;
-		}
-		log.info(destPath.getAbsolutePath());
-		return destPath.getAbsolutePath();
-	}
-
-	public WebElement getElement(By locator) {
-		log.info(locator);
-		if (IsElementPresentQuick(locator))
-			return driver.findElement(locator);
-
-		try {
-			throw new NoSuchElementException("Element Not Found : " + locator);
-		} catch (RuntimeException re) {
-			log.error(re);
-			throw re;
-		}
-	}
-
-	/**
-	 * Check for element is present based on locator If the element is present
-	 * return the web element otherwise null
-	 * 
-	 * @param locator
-	 * @return WebElement or null
-	 */
-
-	public WebElement getElementWithNull(By locator) {
-		log.info(locator);
-		try {
-			return driver.findElement(locator);
-		} catch (NoSuchElementException e) {
-			// Ignore
-		}
-		return null;
-	}
-
-	public boolean IsElementPresentQuick(By locator) {
-		boolean flag = driver.findElements(locator).size() >= 1;
-		log.info(flag);
-		return flag;
-	}
-
 	public WebDriver getBrowserObject(BrowserType bType) throws Exception {
 		try {
-			log.info(bType);
+		//	log.info(bType);
 
 			switch (bType) {
 
 			case Chrome:
-			
+				
 				ChromeBrowser chrome = ChromeBrowser.class.newInstance();
 				return chrome.getChromeDriver(chrome.getChromeCapabilities());
 
-			case Firefox:
-				FirefoxBrowser firefox = FirefoxBrowser.class.newInstance();
-				return firefox.getFirefoxDriver(firefox.getFirefoxCapabilities());
-
-			case HtmlUnitDriver:
-				HtmlUnitBrowser htmlUnit = HtmlUnitBrowser.class.newInstance();
-				return htmlUnit.getHtmlUnitDriver(htmlUnit.getHtmlUnitDriverCapabilities());
-
-			case Iexplorer:
-				IExploreBrowser iExplore = IExploreBrowser.class.newInstance();
-				return iExplore.getIExplorerDriver(iExplore.getIExplorerCapabilities());
-			default:
-				throw new Exception(" Driver Not Found : " + new PropertyFileReader().getBrowser());
 			}
 		} catch (Exception e) {
-			log.equals(e);
+		//	log.equals(e);
 			throw e;
 		}
+		return null;
 	}
 	
 	public void setUpDriver(BrowserType bType) throws Exception 
@@ -195,10 +122,9 @@ public class TestBase {
 		      scenario.embed(screenshot, "image/png"); // ... and embed it in the report.
 		    }
 
-		
-	driver.quit();
-		Thread.sleep(8000);
-		log.info("Browser is closed");
+		 driver.quit();
+		Thread.sleep(2000);
+		//log.info("Browser is closed");
 	}
 	
 	
--- Cucumber/src/main/java/com/cucumber/framework/helper/genericHelper/GenericHelper.java
+++ Cucumber/src/main/java/com/cucumber/framework/helper/genericHelper/GenericHelper.java
@@ -1,66 +1,33 @@
 package com.cucumber.framework.helper.genericHelper;
 
-import org.apache.log4j.Logger;
-import org.openqa.selenium.WebElement;
 
-import com.cucumber.framework.helper.Logger.LoggerHelper;
+import org.openqa.selenium.WebElement;
 import com.cucumber.framework.helper.TestBase.TestBase;
 
 
 public class GenericHelper{
 	
-	private static final Logger log = LoggerHelper.getLogger(GenericHelper.class);
+
+
 	
-	public String readValueFromElement(WebElement element) {
-
-		if (null == element){
-			log.info("weblement is null");
-			return null;
-		}
-
-		boolean displayed = false;
-		try {
-			displayed = isDisplayed(element);
-		} catch (Exception e) {
-			log.error(e);
-			return null;
-		}
-
-		if (!displayed)
-			return null;
-		String text = element.getText();
-		log.info("weblement valus is.."+text);
-		return text;
-	}
-	
-
-	public String readValueFromInput(WebElement element) {
-		if (null == element)
-			return null;
-		if (!isDisplayed(element))
-			return null;
-		String value = element.getAttribute("value");
-		log.info("weblement valus is.."+value);
-		return value;
-	}
 	
 	public boolean isDisplayed(WebElement element) {
 		try {
 			element.isDisplayed();
-			log.info("element is displayed.."+element);
+			
 			return true;
 		} catch (Exception e) {
-			log.info(e);
+			
 			return false;
 		}
 	}
 	public boolean isSelected(WebElement element) {
 		try {
 			element.isSelected();
-			log.info("element is displayed.."+element);
+			
 			return true;
 		} catch (Exception e) {
-			log.info(e);
+			
 			return false;
 		}
 	}
@@ -68,10 +35,10 @@ public class GenericHelper{
 	public boolean isNotDisplayed(WebElement element) {
 		try {
 			element.isDisplayed();
-			log.info("element is displayed.."+element);
+			
 			return false;
 		} catch (Exception e) {
-			log.error(e);
+			
 			return true;
 		}
 	}
@@ -87,14 +54,28 @@ public class GenericHelper{
 
 	public static synchronized String getElementText( WebElement element) {
 		if (null == element) {
-			log.info("weblement is null");
+			
 			return null;
 		}
 		String elementText = null;
 		try {
 			elementText = element.getText();
 		} catch (Exception ex) {
-			log.info("Element not found " + ex);
+			
+		}
+		return elementText;
+	}
+	public static synchronized String getElementTexts( WebElement element) {
+		if (null == element) {
+			
+			return null;
+		}
+		String elementText = null;
+		try {
+			elementText = element.getAttribute("value");
+			System.out.println("Value is correctly displayed");
+		} catch (Exception ex) {
+			
 		}
 		return elementText;
 	}
